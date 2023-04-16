872219ad7871acf8b16437bf3bca6809fc877992
brunafalheiro <brunafalheiro@yahoo.com.br>
2023-04-16 19:11:50 -0300

    Add vlad and candies problem

--- aula-1/vlad-and-candies/vlad-and-candies.py
+++ aula-1/vlad-and-candies/vlad-and-candies.py
@@ -1,18 +1,15 @@
-def canEatCandies(candies):
-  if max(candies) % sum(candies) == 0:
-    print("YES")
-    return
-  if max(candies) <= (sum(candies) - max(candies)) % 2:
-    print("YES")
-  else:
-    print("NO")
 
-# The number of test cases
-t = int(input())
-
-for i in range(t):
-  # The number of types of candies
-  n = int(input())
-  # The number of candies for each type. Turns an input like '1 1 1' into [1,1,1]
-  candies = list(map(int, input().split()))
-  print(canEatCandies(candies))
+n = int(input())
+for _ in range(n):
+  totalCandies = int(input())
+  candieTypes = list(map(int, input().split()))
+  mostFrequentCandie = max(candieTypes)
+  index = candieTypes.index(mostFrequentCandie)
+  if mostFrequentCandie == 1:
+    print("YES")
+    continue
+  for i in range(totalCandies):
+    canEat = candieTypes[i] == mostFrequentCandie and i != index or candieTypes[i] == mostFrequentCandie - 1
+    if canEat:
+      break
+  print("YES" if canEat else "NO")
